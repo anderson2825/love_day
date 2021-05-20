dddf4374dbf0f05c8b6a52ecce63f78fa3a68b3b
anderson <deveil2825@163.com>
2021-05-20 21:11:40 +0800

    moblie
    moblie

--- lovedays/js/index.js
+++ lovedays/js/index.js
@@ -45,3 +45,25 @@ function timer_start(){
 window.setInterval(timer_start,1000);
 timer_start();
 init();
+
+    var audio_music=document.getElementById('audio_music'); 
+    var img_music=document.getElementById('img_music');
+        var timeout_music;
+        function music_switch(){ //切换   
+            clearTimeout(timeout_music);  
+            if(audio_music.paused){
+                console.log('switch music to play');
+                audio_music.play();
+               // audio_record.pause(); //播放音乐时录音一定暂停                
+                img_music.style.webkitAnimation="music_play_rotate 1s linear infinite";
+                $(".div_music_tips").html("正播放").show();                
+                timeout_music=setTimeout(function(){$(".div_music_tips").hide()}, 2500);
+            }else{
+                console.log('switch music to paused'); 
+                audio_music.pause();
+                // audio_record.play(); //
+                img_music.style.webkitAnimation="";
+                $(".div_music_tips").html("已暂停").show();  
+                timeout_music=setTimeout(function(){$(".div_music_tips").hide()}, 2500); 
+            } 
+        }
